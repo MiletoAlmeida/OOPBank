b1fdcf3831587de61f445241cc63befa54e3189c
Mileto Almeida <almeidamileto@gmail.com>
2025-01-02 19:54:29 -0300

    feat: add Account entity class with JPA annotations
    - Added `Account` entity class with JPA annotations
    - Included fields for account details such as id, number, agency, balance, and creditLimit
    - Added getter and setter methods for all fields

---
+++ src/main/java/model/Account.java
@@ -0,0 +1,62 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private long number;
+
+    private int agency;
+
+    private double balance;
+
+    private double creditLimit;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+
+    public int getAgency() {
+        return agency;
+    }
+
+    public void setAgency(int agency) {
+        this.agency = agency;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public void setCreditLimit(double creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+}
