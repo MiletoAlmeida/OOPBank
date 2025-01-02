422d9836942dba7c6703015d59f3380a3262e730
Mileto Almeida <almeidamileto@gmail.com>
2025-01-02 20:33:53 -0300

    chore: Moved OOPBank.java to a new location for better project structure

---
+++ src/main/java/OOPBank.java
@@ -0,0 +1,8 @@
+import model.User;
+
+public class OOPBank {
+    public static void main(String[] args) {
+        System.out.println("Welcome to OOPBank!");
+        User user = new User();
+    }
+}
