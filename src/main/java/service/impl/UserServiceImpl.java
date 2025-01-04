f9e387a29c957e9b31457ee4e25cd5afc6ec39a1
Mileto Almeida <almeidamileto@gmail.com>
2025-01-04 15:55:35 -0300

    feat: implement UserServiceImpl class

---
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package service.impl;
+
+import model.User;
+import service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public Long save(Long entity) {
+        return 0L;
+    }
+
+    @Override
+    public Long update(Long entity) {
+        return 0L;
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public Long findById(User user) {
+        return 0L;
+    }
+
+    @Override
+    public List<Long> findAll() {
+        return List.of();
+    }
+}
