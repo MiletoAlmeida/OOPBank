503475615df1048d8a3706388990bfea5c403875
Mileto Almeida <almeidamileto@gmail.com>
2025-03-16 17:00:22 -0300

    feat: Add AccountNotFoundException for handling account not found errors

---
+++ src/main/java/com/miletodev/oopbank/exception/AccountNotFoundException.java
@@ -0,0 +1,22 @@
+package com.miletodev.oopbank.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AccountNotFoundException extends RuntimeException {
+    public AccountNotFoundException(String message) {
+        super(message);
+    }
+}
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<String> handleAccountNotFound(AccountNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
